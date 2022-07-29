4a7f8337f69cffa93c9ebd0c751e5881b92912d5
Julian <code@sevenfourfour.net>
2022-07-29 06:40:27 -0500

    Create callback function for operations

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ function divide() {
     return (Math.floor((arguments[0] / arguments[1]) * 1000) / 1000)
 }
 
+function operate(callback, num1, num2) {
+    return callback(num1, num2)
+}
+
 console.log(add(1, 6))
 console.log(subtract(2, 9))
 console.log(multiply(2, 5))
 console.log(divide(2, 6))
+
+console.log(operate(divide, 1, 2))
